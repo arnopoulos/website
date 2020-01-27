e260e1613c25d40002d8f93299f84ce1c7e6e444
Andrew Arnopoulos <andrew@arnopoulos.io>
2020-01-27 01:15:27 -0700

    Updates

---
+++ precache-manifest.eef0f23550ee7d95378a458f62eba926.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7ffd894512c00cd7e68d9377e00bf95",
+    "url": "/index.html"
+  },
+  {
+    "revision": "75e4bcb9bb7458e49cd4",
+    "url": "/static/css/main.1fcebbe0.chunk.css"
+  },
+  {
+    "revision": "ef9804a867f77f9e15ac",
+    "url": "/static/js/2.ec1606be.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.ec1606be.chunk.js.LICENSE"
+  },
+  {
+    "revision": "75e4bcb9bb7458e49cd4",
+    "url": "/static/js/main.0e55eed5.chunk.js"
+  },
+  {
+    "revision": "6e7d85ad969107dd34f7",
+    "url": "/static/js/runtime-main.54edee9e.js"
+  },
+  {
+    "revision": "afa3f339b519846f1e79f0e626d546eb",
+    "url": "/static/media/avatar.afa3f339.jpg"
+  }
+]);
