90634b674a82777f7cc93e66e1f703d9e37c3d40
Andrew Arnopoulos <andrew@arnopoulos.io>
2020-02-03 00:07:50 -0700

    More testing with deploy files

--- src/App.js
+++ src/App.js
@@ -4,13 +4,11 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="Avatar" alt="logo"/>
-        <sub className="Subtitle">Just a developer who needed a website for reasons. </sub>
-      </header>
-    </div>
-  );
+    <header className="App-header">
+      <img src={logo} className="Avatar" alt="logo"/>
+      <sub className="Subtitle">Just a developer who needed a website for reasons.</sub>
+    </header>
+);
 }
 
 export default App;
